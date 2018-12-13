1197d82e0e9b49b1e43bb801157a7f9fddb4b94b
Prateek <prateek15mishra@gmail.com>
2018-12-14 01:05:50 +0530

    Intermediate Test commit

--- src/test/java/com/prateek/maxFoodSatifactionProblem/rest/RestHandlerTest.java
+++ src/test/java/com/prateek/maxFoodSatifactionProblem/rest/RestHandlerTest.java
@@ -7,11 +7,8 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.MediaType;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
@@ -19,26 +16,28 @@ import org.springframework.web.context.WebApplicationContext;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.prateek.maxFoodSatifactionProblem.App;
 
+@RunWith(SpringJUnit4ClassRunner.class)
 @WebAppConfiguration
-@RunWith(SpringRunner.class)
-@SpringBootTest(webEnvironment = WebEnvironment.MOCK)
+@ContextConfiguration(classes = App.class)
+
 public class RestHandlerTest {
 
+	@Autowired
 	private MockMvc mockMvc;
 
-	@MockBean
 	private RestHandler restHandler;
 
 	@Autowired
 	private WebApplicationContext webApplicationContext;
-	
+
 	@Autowired
 	private ObjectMapper objectMapper;
 
 	@Before
-	public void setUp() {
-		mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+	public void setup() {
+		this.mockMvc = MockMvcBuilders.webAppContextSetup(this.webApplicationContext).build();
 	}
 
 	@Test
@@ -46,9 +45,7 @@ public class RestHandlerTest {
 
 		String input = "7 4" + "\n 1 1" + "\n4 3" + "\n5 4" + "\n7  5";
 
-		mockMvc.perform(
-				post("/clients").contentType(MediaType.TEXT_PLAIN_VALUE).content(objectMapper.writeValueAsBytes(input)))
-				.andExpect(status().isOk());
+		mockMvc.perform(post("/clients").content(objectMapper.writeValueAsBytes(input))).andExpect(status().isOk());
 	}
 
 }
