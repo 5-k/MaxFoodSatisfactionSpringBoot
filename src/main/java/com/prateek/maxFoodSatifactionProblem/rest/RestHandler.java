6e32b589f6741855fda49106a3ce428bfc1ce965
Prateek Mishra <prateek15mishra@gmail.com>
2018-12-14 09:48:10 +0530

    Removed code that was not needed

--- src/main/java/com/prateek/maxFoodSatifactionProblem/rest/RestHandler.java
+++ src/main/java/com/prateek/maxFoodSatifactionProblem/rest/RestHandler.java
@@ -1,5 +1,8 @@
 package com.prateek.maxFoodSatifactionProblem.rest;
 
+import com.prateek.maxFoodSatifactionProblem.dto.Response;
+import com.prateek.maxFoodSatifactionProblem.exceptions.CustomAPIException;
+import com.prateek.maxFoodSatifactionProblem.service.FoodSatisfactionCalculatorService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -9,33 +12,26 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.prateek.maxFoodSatifactionProblem.dto.Response;
-import com.prateek.maxFoodSatifactionProblem.exceptions.CustomAPIException;
-import com.prateek.maxFoodSatifactionProblem.service.FoodSatisfactionCalculatorService;
 
 @RestController
 public class RestHandler {
 
-	@Autowired
-	private FoodSatisfactionCalculatorService foodSatisfactionCalculatorService;
+    @Autowired
+    private FoodSatisfactionCalculatorService foodSatisfactionCalculatorService;
 
-	@RequestMapping("/test")
-	public String test() {
-		return "Test";
-	}
 
-	@RequestMapping(value = "/app/calculateFoodSatisfactionValue", method=RequestMethod.POST, consumes= MediaType.TEXT_PLAIN_VALUE )
-	public ResponseEntity<Response> getMaximumSatisFactionValue(@RequestBody String input) {
-		
-		try {
-			int response = foodSatisfactionCalculatorService.maximumSatisfaction(input);
-			String message = "John can a satisfaction of value " + response;
-			return ResponseEntity.status(HttpStatus.OK).body(new Response(message, HttpStatus.OK.value()));
-		} catch (CustomAPIException e) {
-			String message = "Well john! Seems like you gave me an invalid input : " + e.getMessage();
-			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new Response(message, HttpStatus.BAD_REQUEST.value()));
-		} catch (Exception e) {
-			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new Response(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR.value()));
-		}
-	}
+    @RequestMapping(value = "/app/calculateFoodSatisfactionValue", method = RequestMethod.POST)
+    public ResponseEntity<Response> getMaximumSatisFactionValue(@RequestBody String input) {
+
+        try {
+            int response = foodSatisfactionCalculatorService.maximumSatisfaction(input);
+            String message = "John can a satisfaction of value " + response;
+            return ResponseEntity.status(HttpStatus.OK).body(new Response(message, HttpStatus.OK.value()));
+        } catch (CustomAPIException e) {
+            String message = "Well john! Seems like you gave me an invalid input : " + e.getMessage();
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new Response(message, HttpStatus.BAD_REQUEST.value()));
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new Response(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR.value()));
+        }
+    }
 }
